59f5e5c130a44fb821ebd39904d5a06fc3a54507
Eric Liang <xxliang@uwaterloo.ca>
2021-11-27 16:40:55 -0800

    README, code, examples added

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # IsolateMusicSymbols
 Filters out everything on a music score except certain provided music symbols.
+
+Created for fun, learning, escaping boredom, and mild algorithm designing practice.
+
+## Prerequisites
+Python 3 is required.
+
+To set up virtual environment:
+```python
+pip3 install virtualenv
+virtualenv -p python3 venv3
+source venv3/bin/activate
+```
+
+To install required libraries in virtual environment:
+```python
+pip3 install -r requirements.txt
+```
+
+## Terms
+- The _base_ image refers to the music score. It has the accidentals and any other symbols you want to isolate.
+- The _template_ image(s) are the **same resolution** cropped image(s) from the *base* image encapsulating a symbol you want to isolate.
+- The _error_threshold_ is the proportion of error in the matching between the _template_ and *base* image that is permitted. See [method](#method) for more detail.
+- The _base_error_weight_ is the weight placed on pixels where the *base* region does not match the *template* region. See [method](#method) for more detail. Typically smaller than _template_error_weight_.
+- The _template_error_weight_ is the weight placed on pixels where the *template* region does not match the *base* region. See [method](#method) for more detail. Typically larger than _base_error_weight_.
+
+## Method
+1. Transform *base* and *template* images in binary 2D array representation.
+2. Convolve (slide) the smaller *template* images over the *base* image and calculate a statistic:
+   1. Perform `xor` (exclusive OR) between the *base* and *template* position. This results in an array that is `1` whenever the images do not match.
+   2. From experimentation, it has been found that the *base* region not matching the *template* region is more important than the *template* region not matching the *base* region. Therefore, weight the former higher.
+   3. Sum all the values after the transformation. If the `sum < error_threshold`, then imprint the *template* image onto a fresh composite layer.
+3. Perform `or` on all composites to obtain the final image.
+
+## Usage
+
+Configuration is provided through a `config.json` file, or anything you name it as. The default configuration file path is `./config.json`.
+
+1. Manual image preparation
+   - From the *base* image, **crop out** (*do not screenshot!*) *template* images with at least a 1 pixel white border.
+2. Configuration in `config.json`
+   - Configure the *base* image processing under the key `config["base_image"]`.
+   - Configure the *template* image processing in a list under the key `config["templates"]`.
+   - See [configuration fields](#configuration-fields) below on filling the configuration fields.
+3. Call `python3 isolate_music_symbols.py [--config <config_path>]`
+
+### Configuration Fields
+- `write_path` (str): Path where the output image will be written.
+- `base_image` (dict): *Base* image configuration.
+  - `file_path` (str): Path to *base* image for reading.
+  - `blur_kernel_size` (int, optional): Amount of blur to apply to the *base* image. Must be odd positive integer. Recommended: (1 or 3). Default: 1.
+  - `threshold` (int, optional): Threshold used for converting greyscale to black-and-white image. Values below threshold become 0 (black) and values above threshold become 255 (white). Default: 180.
+- `templates` (list):
+  - (dict):
+    - `file_path` (str): Path to *template* image for reading.
+    - `blur_kernel_size` (int, optional): Amount of blur to apply to the *template* image. Must be odd positive integer. Recommended: (1 or 3). Default: 1.
+    - `threshold` (int, optional): Threshold used for converting greyscale to black-and-white image. Values below threshold become 0 (black) and values above threshold become 255 (white). Default: 180.
+    - `method` (str): Default is `"xor_weighted"`. No other method is supported at the moment.
+    - `method_parameters` (dict):
+      - `error_threshold` (float): See [terms](#terms). Recommended: [0.1, 0.3].
+      - `base_error_weight` (int, optional): See [terms](#terms). Recommended: 1. Default: 1.
+      - `template_error_weight` (int, optional): See [terms](#terms). Recommended: {2, 3}. Default: 1.
+
+## Example
+In the `example` directory, `example/config.json` provides a working example of appropriate configuration values for the *base_image* `example/test_image.png`.
+
+Call `python3 isolate_music_symbols.py --config example/config.json` to try. Open `result.png` when finished to see result.
---
+++ isolate_music_symbols.py
@@ -0,0 +1,125 @@
+print("Loading libraries...")
+import argparse
+import json
+from time import time
+
+import cv2
+import numpy as np
+from joblib import Parallel, delayed
+from numpy.lib.stride_tricks import sliding_window_view
+
+
+class DEFAULTS:
+    CONFIG_FILE_PATH = "config.json"
+    BLUR_KERNEL_SIZE = 1
+    THRESHOLD = 180
+    BASE_ERROR_WEIGHT = 1
+    TEMPLATE_ERROR_WEIGHT = 1
+
+
+def prepare_image(image_name: str, blur_kernel_size: int, threshold: int, bin_scale: bool = True) -> np.ndarray:
+    assert (
+        blur_kernel_size % 2 == 1
+    ) and blur_kernel_size > 0, "Guassian blur kernel size must be an odd positive number"
+    assert threshold >= 0 and threshold <= 255, "Threshold must be between 0 and 255 inclusive"
+    # Load image as 2D grayscale
+    base_image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+    # Maximize contrast
+    base_image = cv2.convertScaleAbs(base_image, alpha=3.0, beta=0.0)
+    # Blur the image slightly if details are too fine
+    base_image = cv2.GaussianBlur(base_image, (blur_kernel_size, blur_kernel_size), cv2.BORDER_DEFAULT)
+    # Convert to black-and-white image (i.e. values are either 0 or 255)
+    base_image = cv2.threshold(base_image, threshold, 255, cv2.THRESH_BINARY)[1]
+    # Scale and invert the image into binary form (i.e. values are either 0 or 1)
+    return 1 - (base_image / 255) if bin_scale else base_image
+
+
+def convolve_template_xor(
+    base: np.ndarray,
+    template: np.ndarray,
+    error_threshold: float,
+    base_error_weight: int,
+    template_error_weight: int,
+) -> np.ndarray:
+    base_h, base_w = base.shape
+    template_h, template_w = template.shape
+
+    # Maximum error allowable based on weights
+    base_max_error = (
+        (template_h * template_w * base_error_weight) + np.sum(template) * (template_error_weight - base_error_weight)
+    ) * error_threshold
+    # Pre-computed mask to apply weights
+    error_weight = (template * (template_error_weight - base_error_weight)) + base_error_weight
+
+    composite = np.zeros(base.shape)
+    # Gets all convolution windows efficiently
+    base_windows = sliding_window_view(base, template.shape)
+    for h in range(base_h - template_h + 1):
+        for w in range(base_w - template_w + 1):
+            if np.sum(cv2.multiply(cv2.bitwise_xor(base_windows[h][w], template), error_weight)) < base_max_error:
+                composite[h : h + template_h, w : w + template_w] = cv2.bitwise_or(
+                    composite[h : h + template_h, w : w + template_w], template
+                )
+    return composite
+
+
+def process(args: argparse.Namespace):
+    config_path = args.config or DEFAULTS.CONFIG_FILE_PATH
+    print(f"Using {config_path} as config...")
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    base_image = config["base_image"]
+    base_image_prepared = prepare_image(
+        base_image["file_path"],
+        blur_kernel_size=base_image.get("blur_kernel_size", DEFAULTS.BLUR_KERNEL_SIZE),
+        threshold=base_image.get("threshold", DEFAULTS.THRESHOLD),
+    )
+
+    print("Preparing templates for processing...")
+    templates_prepared = (
+        (
+            prepare_image(
+                template_image["file_path"],
+                blur_kernel_size=template_image.get("blur_kernel_size", DEFAULTS.BLUR_KERNEL_SIZE),
+                threshold=template_image.get("threshold", DEFAULTS.THRESHOLD),
+            ),
+            template_image["method"],
+            template_image["method_parameters"],
+        )
+        for template_image in config["templates"]
+    )
+
+    print("Generating composites...")
+    start_time = time()
+    composites = Parallel(n_jobs=len(config["templates"]))(
+        delayed(convolve_template_xor)(
+            base_image_prepared,
+            template_prepared,
+            error_threshold=method_parameters["error_threshold"],
+            base_error_weight=method_parameters.get("base_error_weight", DEFAULTS.BASE_ERROR_WEIGHT),
+            template_error_weight=method_parameters.get("template_error_weight", DEFAULTS.TEMPLATE_ERROR_WEIGHT),
+        )
+        for template_prepared, _, method_parameters in templates_prepared
+    )
+    print(f"Completed in {time() - start_time} seconds.")
+
+    print("Merging together composites...")
+    output_image = np.zeros(base_image_prepared.shape)
+    for composite in composites:
+        output_image = cv2.bitwise_or(output_image, composite)
+    output_image = (1 - output_image) * 255
+
+    print(f"Writing output image to {config['write_path']}")
+    cv2.imwrite(config["write_path"], output_image)
+
+
+def get_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--config", required=False, type=str, help="Path to configuration file")
+    return arg_parser.parse_args()
+
+
+if __name__ == "__main__":
+    cmd_args = get_args()
+    process(cmd_args)
